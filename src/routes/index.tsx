f20d792db40a1e4e6f1035020e0f0b9d6316947f
Fynn <fynn@fynnyoung.com>
2025-10-23 14:37:04 +0200

    get api key in index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const apiKey = process.env.API_KEY;
 
 export const Route = createFileRoute('/')({
   component: App,
