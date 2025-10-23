458c0142c22ddf32512ac5478ba08dfcc8088d0e
Fynn <fynn@fynnyoung.com>
2025-10-23 14:18:54 +0200

    remove example code

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen.ts'
 
-import './styles.css'
-
 // Create a new router instance
 const router = createRouter({
   routeTree,
