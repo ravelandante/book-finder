012081fdbf517b32b553a246a55f608bca67fb59
Fynn <fynn@fynnyoung.com>
2025-10-23 14:49:47 +0200

    create basic form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router';
 import dotenv from 'dotenv';
+import { useForm } from 'react-hook-form';
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
 
 export const Route = createFileRoute('/')({
   component: App,
-})
+});
+
+type Inputs = {
+  bookName: string;
+};
 
 function App() {
-  return <div></div>
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('bookName', { required: true })} />
+
+      <input type="submit" />
+    </form>
+  );
 }
